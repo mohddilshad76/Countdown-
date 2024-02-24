9ec2d7e3c86bbac4acc40e02006234d3b7435219
Dilshad Shaikh <80386441+mohddilshad76@users.noreply.github.com>
2024-02-24 17:36:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 29, 2020',
-  name: 'Darlene'
+  birthdate: 'Feb 25, 2024',
+  name: 'Muskan'
 };
 
 function hideEverything() {
